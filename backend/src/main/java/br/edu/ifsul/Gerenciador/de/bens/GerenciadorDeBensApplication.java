33c71c905e8c8976e24e315bd1ef29de0cbfe05c
marcosschlick <marcosschlick@gmail.com>
2024-11-22 23:05:46 -0300

    add spring and react files

---
+++ backend/src/main/java/br/edu/ifsul/Gerenciador/de/bens/GerenciadorDeBensApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.Gerenciador.de.bens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeBensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeBensApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/ifsul/Gerenciador/de/bens/GerenciadorDeBensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.Gerenciador.de.bens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeBensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
